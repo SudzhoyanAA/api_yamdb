7654fe0a4f37312ba5a55ae03d0cae2c847524a2
Альберт Суджоян <asudzhoyan@bk.ru>
2023-10-07 19:27:58 +0300

    modified:   api_yamdb/api_yamdb/settings.py

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -8,7 +8,10 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('KEY')
+SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
+# SECRET_KEY = os.getenv('KEY') тесты ругаются(((((
+# django.core.exceptions.ImproperlyConfigured: The SECRET_KEY setting must not be empty.
+# и ругаются именно на платформе яндекса. А так я сделал, честно)))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
